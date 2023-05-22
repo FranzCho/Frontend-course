75fa57e83936f6d06ec8982ca3ba5334f23eb254
Franz Cho <franzzcho@gmail.com>
2023-05-23 02:59:22 +0900

    feat: setState

---
+++ class/pages/section07/07-01/index.js
@@ -0,0 +1,10 @@
+import BoardWrite from '@/components/units/board/07-write/BoardWrite.container';
+
+export default function GraphqlMutationPage() {
+
+  return (
+    <>
+      <BoardWrite />
+    </>
+  )
+}
