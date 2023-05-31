1465f83ac9212d9acc1e29805560ab8bb53b8200
Franz Cho <franzzcho@gmail.com>
2023-05-31 19:26:36 +0900

    feat: ESLint & prettier

--- class/pages/_app.tsx
+++ class/pages/_app.tsx
@@ -1,17 +1,15 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
-import { AppProps } from 'antd'
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import type { AppProps } from "next/app";
 
-
-export default function App({ Component } : AppProps) {
-
+export default function App({ Component }: AppProps): JSX.Element {
   const client = new ApolloClient({
     uri: "http://backend-example.codebootcamp.co.kr/graphql",
-    cache: new InMemoryCache() // 컴퓨터의 메모리에다가 백엔드에서 받아온 데이터 임시로 저장해 놓기 => 나중에 더 자세히 알아보기
-  })
+    cache: new InMemoryCache(), // 컴퓨터의 메모리에다가 백엔드에서 받아온 데이터 임시로 저장해 놓기 => 나중에 더 자세히 알아보기
+  });
 
   return (
     <ApolloProvider client={client}>
       <Component />
     </ApolloProvider>
-  )
+  );
 }
